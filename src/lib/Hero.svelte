1cc2444ac4d54fc0888c934cb7ea4ea49436a7e1
bswck <bartosz@slawecki.dev>
2025-06-16 21:56:01 +0200

    Fixes from Schizoid

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -6,10 +6,10 @@
 <Section border={false} class="gap-8 select-none">
     <div class="flex flex-col items-center justify-center gap-8 text-center [&>*]:hover:scale-110 [&>*]:transition [&>*]:duration-1000">
         <div class="invert-100">
-        <img class="h-48" src="/logo.png" alt="" />
+        <img class="h-36" src="/logo.png" alt="" />
         </div>
-        <nav aria-roledescription="Links to socials" class="flex items-center w-56 justify-between [&>*]:text-4xl">
-            <Social url="/discord" icon="fa-brands:discord" class="text-4xl" />
+        <nav aria-roledescription="Links to socials" class="flex items-center w-44 justify-between [&>*]:text-3xl">
+            <Social url="/discord" icon="fa-brands:discord" />
             <Social url="https://github.com/nawczoraj" icon="fa-brands:github" />
             <Social url="https://linkedin.com/company/nawczoraj" icon="fa-brands:linkedin" />
         </nav>
