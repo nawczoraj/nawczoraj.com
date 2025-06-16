5e2ee088b977eaaf0ed514616b393420889d4489
bswck <bartosz@slawecki.dev>
2025-06-16 21:32:33 +0200

    Scale

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -4,14 +4,14 @@
 </script>
 
 <Section border={false} class="gap-8 select-none animate-fade-in, motion-safe, transition-">
-    <div class="flex flex-col items-center justify-center gap-6 text-center [&>*]:hover:scale-110 [&>*]:transition [&>*]:duration-1000">
+    <div class="flex flex-col items-center justify-center gap-8 text-center [&>*]:hover:scale-110 [&>*]:transition [&>*]:duration-1000">
         <div class="invert-100">
-        <img class="h-16" src="/logo.png" alt="" />
+        <img class="h-48" src="/logo.png" alt="" />
         </div>
-        <nav aria-roledescription="Links to socials" class="flex items-center min-w-30 w-full justify-evenly">
-            <Social url="/discord" icon="fa-brands:discord" class="text-xl" />
-            <Social url="https://github.com/nawczoraj" icon="fa-brands:github" class="text-xl" />
-            <Social url="https://linkedin.com/company/nawczoraj" icon="fa-brands:linkedin" class="text-xl" />
+        <nav aria-roledescription="Links to socials" class="flex items-center w-56 justify-between [&>*]:text-4xl">
+            <Social url="/discord" icon="fa-brands:discord" class="text-4xl" />
+            <Social url="https://github.com/nawczoraj" icon="fa-brands:github" />
+            <Social url="https://linkedin.com/company/nawczoraj" icon="fa-brands:linkedin" />
         </nav>
     </div>
 </Section>
